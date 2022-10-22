392464efb7809da01cda31df5b5a301e91984f1f
Emi <emilianogarciaokk@gmail.com>
2022-10-22 17:40:38 -0300

    push styles

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import styles from "./landingpage.module.css";
+import {Link} from 'react-router-dom'
+import pokelanding from '../../assets/pokeLan.webp'
+import pokebaile from '../../assets/pokebaile.gif'
+
 
 export default function LandingPage() {
 return(
  <div className={styles.landing}>
 <div>
-    <button>Press Start!</button>
+<h1 className={styles.h1}>Welcome to PokeApp</h1>
+<p className={styles.flecha}>⇣</p>
+<img src={pokebaile} className={styles.pokebaile} alt="" />
+<Link to='/home'>
+<img src={pokelanding} className={styles.botonLan} alt="" />
+</Link>
+
 </div>
  </div> 
 )
