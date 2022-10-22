79ec2eca7f2e6afd587e3d757c84c06f0b52b17e
Emi <emilianogarciaokk@gmail.com>
2022-10-22 15:16:15 -0300

    push1

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ const { conn } = require('./src/db.js');
 // Syncing all the models at once.
 conn.sync({ force:true }).then(() => {
   const Port=process.env.PORT||3001
-  server.listen(Port, () => {
+  server.listen(process.env.PORT||3001, () => {
     console.log(Port); // eslint-disable-line no-console
   });
 });
